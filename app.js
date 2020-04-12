30c179192b78e70fca89a9cf5cc9d646c16319f4
dekimioc <dekimioc@yahoo.com>
2020-04-12 12:35:22 +0200

    ejs templating engine

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const bodyParser = require('body-parser');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars'); requiering express-handlebars
 
 const app = express();
 
-app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout' }));
-app.set('view engine', 'handlebars');         // Implementing handlebars templating engine
+// app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout' }));                // registering express-handlebars
+//app.set('view engine', 'handlebars');        // Implementing handlebars templating engine
 // app.set('view engine', 'pug');     // Implementing pug templating engine
+app.set('view engine', 'ejs');        // Implementing ejs templating engine
 app.set('views', 'views');
 
 app.use(express.static(path.join(rootDir, 'public')));
