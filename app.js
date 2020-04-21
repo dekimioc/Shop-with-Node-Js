56fa12fb66747b594732b54c00009f0f7e0996f6
dekimioc <dekimioc@yahoo.com>
2020-04-21 22:14:53 +0200

    database and mysql2 installed

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const path = require('path');
 
-const rootDir = require('./helpers/path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const expressHbs = require('express-handlebars'); requiering express-handlebars
 
 const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine('handlebars', expressHbs({ layoutsDir: 'views/layouts', defaultLayout: 'main-layout' }));                // registering express-handlebars
-//app.set('view engine', 'handlebars');        // Implementing handlebars templating engine
-// app.set('view engine', 'pug');     // Implementing pug templating engine
 app.set('view engine', 'ejs');        // Implementing ejs templating engine
 app.set('views', 'views');
 
-app.use(express.static(path.join(rootDir, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
@@ -28,4 +22,4 @@ app.use(shopRoutes);
 
 app.use(errorController.get404Page)
 
-app.listen(5000);
+app.listen(3000);
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,87 +2,90 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', {
-            prods: products,
-            pageTitle: 'All Products',
-            path: "/"
-            // hasProducts: products.length > 0,
-            // activeShop: true,                   // This three lines are used for handlebars templating engine
-            // productCss: true
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: "/"
+            });
+        })
+        .catch(err => {
+            console.log(err)
         });
-    });
 }
 
 exports.getProduct = (req, res, next) => {
     const productId = req.params.productId;
-    Product.findById(productId, product => {
-        res.render('shop/product-detail', {
-            product: product,
-            path: "/products",
-            pageTitle: product.title
-        })
-    })
-}
+    Product.findById(productId)
+        .then(([product]) => {
+            res.render('shop/product-detail', {
+                product: product[0],
+                path: "/products",
+                pageTitle: product.title
+            })
+        }).catch(err => console.log(err));
+};
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/index', {
-            prods: products,
-            pageTitle: 'Shop',
-            path: "/products"
-            // hasProducts: products.length > 0,
-            // activeShop: true,                       // This three lines are used for handlebars templating engine
-            // productCss: true
+    Product.fetchAll()
+        .then(([rows, fieldData]) => {
+            res.render('shop/product-list', {
+                prods: rows,
+                pageTitle: 'All Products',
+                path: "/"
+            });
+        })
+        .catch(err => {
+            console.log(err)
         });
-    });
 }
 
 exports.getCart = (req, res, next) => {
-    Cart.getCart(cart => {
-        Product.fetchAll(products => {
-            const cartProducts = [];
-            for (product of products) {
-                const cartProductData = cart.products.find(prod => prod.id === product.id);
-                if (cartProductData) {
-                    cartProducts.push({ productData: product, qty: cartProductData.qty })
-                }
-            }
-            res.render('shop/cart', {
-                path: '/cart',
-                pageTitle: 'Your Cart',
-                products: cartProducts
-            })
-        })
-    })
+    // Cart.getCart(cart => {
+    //     Product.fetchAll(products => {
+    //         const cartProducts = [];
+    //         for (product of products) {
+    //             const cartProductData = cart.products.find(prod => prod.id === product.id);
+    //             if (cartProductData) {
+    //                 cartProducts.push({ productData: product, qty: cartProductData.qty })
+    //             }
+    //         }
+    //         res.render('shop/cart', {
+    //             path: '/cart',
+    //             pageTitle: 'Your Cart',
+    //             products: cartProducts
+    //         })
+    //     })
+    // })
 }
 
 exports.postDeleteProdcutFromCart = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.findById(prodId, product => {
-        Cart.deleteProduct(prodId, product.price);
-        res.redirect('/cart');
-    })
+    // const prodId = req.body.productId;
+    // Product.findById(prodId, product => {
+    //     Cart.deleteProduct(prodId, product.price);
+    //     res.redirect('/cart');
+    // })
 }
 
 exports.postProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.findById(prodId, (product) => {
-        Cart.addProduct(prodId, product.price);
-    })
-    res.redirect('/cart');
+    // const prodId = req.body.productId;
+    // Product.findById(prodId, (product) => {
+    //     Cart.addProduct(prodId, product.price);
+    // })
+    // res.redirect('/cart');
 }
 
 exports.getOrders = (req, res, next) => {
-    res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders'
-    })
+    // res.render('shop/orders', {
+    //     path: '/orders',
+    //     pageTitle: 'Your Orders'
+    // })
 }
 
 exports.getCheckout = (req, res, next) => {
-    res.render('shop/checkout', {
-        path: '/checkout',
-        pageTitle: 'Checkout'
-    })
+    // res.render('shop/checkout', {
+    //     path: '/checkout',
+    //     pageTitle: 'Checkout'
+    // })
 }
